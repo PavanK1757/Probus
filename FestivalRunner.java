0c63a8215d4decacec7618c48550914bf38852c9
pavankumar-0114 <pavankxworkz098@gmail.com>
2025-08-12 16:17:59 +0530

    task

---
+++ FestivalRunner.java
@@ -0,0 +1,12 @@
+class FestivalRunner {
+	public static void main(String[]args){
+
+        String Name=Festival.Festivalname("Navaratri");
+		int Days=Festival.noOfDays(9);
+        char letter=Festival.Month('O');
+        boolean Food=Festival.isDelicious(true);
+        double star=Festival.rating(9.2);
+//       
+        
+		}
+}
